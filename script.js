175ca988991c25ac9dd1d56cc91fc8a60ef77fe8
kagonameshack-cmyk <kagonameshack@gmail.com>
2026-04-19 16:24:59 +0300

    script.js

--- script.js
+++ script.js
@@ -223,3 +223,17 @@ navMenu.classList.toggle("active");
 });
 
 });
+
+
+window.addEventListener("scroll", function(){
+
+const header = document.querySelector("header");
+
+if(window.scrollY > 50){
+header.classList.add("scrolled");
+}
+else{
+header.classList.remove("scrolled");
+}
+
+});
